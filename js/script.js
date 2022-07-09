07ea126b20e9846db4c5caae1978bdd8bd964a4e
Daniel-derf <danielpardinho321@gmail.com>
2022-07-08 23:02:38 -0300

    desenvolvendo o botao pausar

--- js/script.js
+++ js/script.js
@@ -2,11 +2,34 @@ const relogio = document.getElementById("relogio").innerHTML;
 
 var i = 0;
 
-var cont = 0;
-
 var y = 0;
 
-var parar = false;
+var cliques = 0;
+
+
+//start
+var iniciar = document.getElementById("start");
+iniciar.addEventListener("click", inicio);
+iniciar.addEventListener("click", function(){
+    cliques = cliques + 1;
+});
+
+//reset
+var resetar = document.getElementById("reset");
+resetar.addEventListener("click", reset);
+resetar.addEventListener("click", function(){
+    if (cliques > 0){
+        cliques = cliques - 1;}
+});
+
+//pause
+var pausa = document.getElementById("pause");
+pausa.addEventListener("click", pause);
+pausa.addEventListener("click", function(){
+    if (cliques > 0){
+        cliques = cliques - 1;}
+});
+
 
 function waitforme(milisec) {
     return new Promise(resolve => {
@@ -20,32 +43,39 @@ function reset(){
 
 }
 
+function pause(){
+
+    document.getElementById("pause").innerHTML = "PAUSED";
+
+}
+
 async function inicio(){
 
-    cont++;
     
     while (i <=60){
 
-        if ((cont>1) && (y>0)){
-            break;
+        console.log(cliques);
+
+        if (cliques == 1){
+            console.log('If ativado');
+            iniciar.disabled = true;
         }
 
-    
+        if (cliques != 1) {
+            iniciar.disabled = false;
+        }
+
 
         if ((i > 1) && (document.getElementById("relogio").innerHTML == 0+":"+0)){
-
-
             y = 0;
-            i = 0;
-
-            
+            i = 0;    
             break;
-            
-
         }
 
-
-
+        if ((i > 1) && (document.getElementById("pause").innerHTML == "PAUSED")){
+    
+            break;
+        }
 
         document.getElementById("relogio").innerHTML = y+":"+i;
 
@@ -59,8 +89,6 @@ async function inicio(){
         
             y++;
         }
-
-
 
     }
 
